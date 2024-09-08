06304e964c52131e8399fd6b3e064cb14e871de4
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-09-08 17:39:22 -0300

    feat: work repository

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,13 +1,16 @@
 import { AdminRepository } from "./admin-repository";
 import { UserRepository } from "./user-repository";
+import { WorkRepository } from "./work-repository";
 
 class Repository{
     public user: UserRepository;
     public admin: AdminRepository;
+    public work: WorkRepository;
 
     constructor(){
         this.user = new UserRepository();
         this.admin = new AdminRepository()
+        this.work = new WorkRepository()
     }
 }
 
